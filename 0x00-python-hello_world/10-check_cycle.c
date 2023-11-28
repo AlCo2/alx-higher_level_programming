9b34ae94c1947430bc100889b174a1f3bb1a3d01
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-11-28 20:52:06 +0100

    fix betty style

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,13 +1,24 @@
 #include "lists.h"
 
-int check_cycle(listint_t *list){
+/*
+ * check_cycle - function to check if linked list a cycle
+ *
+ * @list: the linked list
+ *
+ * Return: true if a cycle, false if not
+ */
+
+int check_cycle(listint_t *list)
+{
 	listint_t *fixed = list;
 	listint_t *temp = list;
 
-	while(temp){
+	while (temp)
+	{
 		temp = temp->next;
 		if(temp == fixed)
-			return 1;
+			return (1);
 	}
-	return 0;
+	return (0);
 }
+
