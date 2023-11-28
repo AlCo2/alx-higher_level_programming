feabf868e63d0c9b298247d3e603d101a1297d94
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-11-28 22:02:53 +0100

    fix error task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,7 +12,9 @@ int check_cycle(listint_t *list)
 {
 	listint_t *fixed = list;
 	listint_t *temp = list;
-
+	
+	if (list == NULL || list->next == NULL)
+		return (0);
 	while (temp)
 	{
 		temp = temp->next;
