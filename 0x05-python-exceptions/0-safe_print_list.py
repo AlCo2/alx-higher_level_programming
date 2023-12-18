e4ee6cc2644600b2468c98ecbbb3c862e4498596
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-12-18 19:15:25 +0100

    fix check errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,5 +5,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end='')
         except IndexError:
-            return i - 1
+            print("\n", end='')
+            return i
         i += 1
+    print("\n", end='')
+    return i
