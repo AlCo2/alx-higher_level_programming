986b644466f366fb7d150a5181742a721673e04e
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-12-18 20:04:45 +0100

    fix failed checks

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,15 +6,5 @@ def safe_print_division(a, b):
     except:
         return None
     finally:
-        print("{}".format(result))
+        print("Inside result: {}".format(result))
     return result
-
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
