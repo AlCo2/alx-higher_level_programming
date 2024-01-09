7c9c73317178d37db905f4d26ecd968ebeedc5e4
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-01-10 00:45:25 +0100

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    8. Class to JSON
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    function that returns the dictionary description with
+    simple data structure (list, dictionary,
+    string, integer and boolean) for JSON
+    serialization of an object
+    """
+
+    return json.dumps(obj)
