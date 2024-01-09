a69ecc90ba396328e78c43ea40ce926670f7454d
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-01-09 23:37:19 +0100

    fix pycodestyle

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" this is a function that write to a file
+    and return the number of charactere writen
+"""
+
 
 def write_file(filename="", text=""):
     with open(filename, 'w', encoding="utf-8") as f:
