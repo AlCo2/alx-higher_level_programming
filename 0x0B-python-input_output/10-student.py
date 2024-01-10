95607bd9bccea35ad5c575dac5fac339ae7b1766
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-01-10 01:11:31 +0100

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+ 9. Student to JSON
+"""
+
+
+class Student:
+    """
+     class Student that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return {'first_name': self.first_name,
+                'last_name': self.last_name,
+                'age': self.age
+                }
+
+    def to_json(self, attrs=None):
+        dic = {}
+        if not isinstance(attrs, list):
+            return {'first_name': self.first_name,
+                    'last_name': self.last_name,
+                    'age': self.age
+                    }
+        for name in attrs:
+            if name == 'first_name':
+                dic['first_name'] = self.first_name
+            if name == 'age':
+                dic['age'] = self.age
+            if name == 'last_name':
+                dic['last_name'] = self.last_name
+        if (len(dic) > 0):
+            return dic
+        return {'first_name': self.first_name,
+                'last_name': self.last_name,
+                'age': self.age
+                }
+
+
+student_1 = Student("John", "Doe", 23)
+student_2 = Student("Bob", "Dylan", 27)
+
+j_student_1 = student_1.to_json()
+j_student_2 = student_2.to_json(['first_name', 'age'])
+j_student_3 = student_2.to_json(['middle_name', 'age'])
+
+print(j_student_1)
+print(j_student_2)
+print(j_student_3)
