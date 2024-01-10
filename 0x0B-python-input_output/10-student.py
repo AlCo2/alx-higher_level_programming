9a30c425d7d307e3697fe2aa8a5dce16bb2c1d38
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-01-10 01:19:06 +0100

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -17,12 +17,8 @@ class Student:
         dic = {}
         if isinstance(attrs, list):
             for name in attrs:
-                if name == 'first_name':
-                    dic['first_name'] = self.first_name
-                if name == 'age':
-                    dic['age'] = self.age
-                if name == 'last_name':
-                    dic['last_name'] = self.last_name
+                if hasattr(self, name):
+                    dic[name] = getattr(self, name)
         if (len(dic) > 0 and isinstance(attrs, list)):
             return dic
         return self.__dict__
