5d2872c664b26cf6c99be344af1a2852c9a4f31d
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-01-14 02:25:48 +0100

    write unittest for Base class

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 """ unittest for Base class """
 import unittest
+from models.base import Base
+
 
 class TestBase(unittest.TestCase):
     """ class to test The base Clsas """
 
+    def test_init(self):
+        base = Base()
+        self.assertEqual(base.id, 1)
 
+    def test_id(self):
+        base = Base(31)
+        self.assertEqual(base.id, 31)
+
+
+if __name__ == '__main__':
+    unittest.main()
