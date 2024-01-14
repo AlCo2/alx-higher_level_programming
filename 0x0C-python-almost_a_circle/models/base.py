cc76ca871f08d763a68e36e820bb36f365591d8c
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-01-14 01:26:28 +0100

    task 2

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,11 +7,11 @@ class Base:
     base class that will be the base of all other
     classes in this projcet
     """
-
+    __nb_objects = 0
     def __init__(self, id=None):
-        self.__nb_objects = 0
         if id is not None:
             self.id = id
         else:
-            __nb_objects += 1
-            self.id = __nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
