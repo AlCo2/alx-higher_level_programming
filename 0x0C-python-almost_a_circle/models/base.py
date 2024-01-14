cc375c975462b4911343ff76fc89801a9d36339c
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-01-14 02:08:21 +0100

    task 3

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,11 +7,12 @@ class Base:
     base class that will be the base of all other
     classes in this projcet
     """
+
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" unittest for Base class """
+import unittest
+
+class TestBase(unittest.TestCase):
+    """ class to test The base Clsas """
+
+
