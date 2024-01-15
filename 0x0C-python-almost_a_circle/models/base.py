7bdf8a4d41fe64544bc1354388da01d8d245f957
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-01-15 23:43:39 +0100

    fix code style

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,12 +17,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
     @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
-    
+
     @classmethod
     def save_to_file(cls, list_objs):
         with open("{}".format(cls.__name__ + ".json"), 'w') as f:
