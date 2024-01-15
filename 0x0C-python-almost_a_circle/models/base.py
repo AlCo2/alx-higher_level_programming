21bd468fc6ec6df7b508b34ecdb85a5b7e9094c7
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-01-16 00:49:46 +0100

    task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,3 +32,7 @@ class Base:
             else:
                 list_dicts = [o.to_dictionary() for o in list_objs]
                 f.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        return json.loads(json_string)
