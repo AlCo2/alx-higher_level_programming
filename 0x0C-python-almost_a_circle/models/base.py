123db7f49a87f24211d7c0eaf8b1f03c402cf598
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-01-16 00:51:06 +0100

    add handle for empty string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,4 +35,6 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        if json_string is None or json_string = "":
+            return []
         return json.loads(json_string)
