1bf9d2cbd54ab7e29371d9d7a43b335c702cf076
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-01-17 00:23:26 +0100

    documentation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,12 +20,18 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """
+        turn object to json
+        """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """
+        save to a file json
+        """
         with open("{}".format(cls.__name__ + ".json"), 'w') as f:
             if list_objs is None:
                 f.write("[]")
@@ -35,12 +41,18 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        """
+        read from json file
+        """
         if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
+        """
+        create method
+        """
         if dictionary and dictionary != {}:
             if cls.__name__ == "Rectangle":
                 new = cls(1, 1)
@@ -51,6 +63,9 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
+        """
+        load from a file
+        """
         filename = str(cls.__name__) + ".json"
         try:
             with open(filename, "r") as jsonfile:
@@ -61,6 +76,9 @@ class Base:
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
+        """
+        save to a file as csv
+        """
         filename = str(cls.__name__) + ".csv"
         with open(filename, 'w') as f:
             for rect in list_objs:
@@ -72,6 +90,9 @@ class Base:
 
     @classmethod
     def load_from_file_csv(cls):
+        """
+        read from a csv file
+        """
         filename = str(cls.__name__) + ".csv"
         list = []
         with open(filename, 'r') as f:
