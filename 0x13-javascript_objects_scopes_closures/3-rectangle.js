57e8cf52805630e4784d079908c61dc652310be6
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-13 01:53:11 +0000

    change structure

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,9 +7,9 @@ class Rectangle {
 		}
 	}
 	print() {
-		for(let i = 0;i<this.width;i++) {
+		for(let i = 0;i<this.height;i++) {
 			let temp = '';
-			for(let j = 0;j<this.height;j++) {
+			for(let j = 0;j<this.width;j++) {
 				temp += 'X';
 			}
 			console.log(temp);
