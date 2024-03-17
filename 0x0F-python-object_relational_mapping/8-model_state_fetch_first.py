90469eaf08e2cac741e481c66accab8760394770
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-17 21:31:57 +0000

    remove semicolon

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).first();
+    state = session.query(State).first()
     if state is not None:
         print("{}: {}".format(state.id, state.name))
     else:
