2ce96f1b3cb26b7ae9008c9266f0bac2c3b9aa0b
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-17 18:32:26 +0000

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" connect to database and fetch some data """
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host="localhost",
+                         user=username, passwd=password, db=database)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM cities  ORDER BY cities.id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
