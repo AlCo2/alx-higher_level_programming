9f5bd220df1d97085abbfdb8747d4420064042e1
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-17 18:11:58 +0000

    task 3, safe version

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" connect to database and fetch some data """
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+    state = state.replace('"', '')
+    state = state.replace(";", '')
+    state = state.replace("'", '')
+    print(state)
+    db = MySQLdb.connect(host="localhost",
+                         user=username, passwd=password, db=database)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE
+                name LIKE BINARY '{}' ORDER BY states.id""".format(state))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
