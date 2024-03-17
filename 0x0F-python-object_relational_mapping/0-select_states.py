0d6f4a982cd6b95cfe8341b4d2c5d07d5e46e2cb
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-17 17:27:33 +0000

    add order by

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost",
                          user=username, passwd=password, db=database)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
