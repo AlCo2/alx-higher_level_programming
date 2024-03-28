50bad99dff9d1f205ae65d58c2b5cd2058ec5a24
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-28 18:26:05 +0000

    fix task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
     response = requests.get(sys.argv[1])
-    if (response.code >= 400):
-        print('Error code:', response.code)
+    if (response.status_code >= 400):
+        print('Error code:', response.status_code)
     else:
         print(response.text)
