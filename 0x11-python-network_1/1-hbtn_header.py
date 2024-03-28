813c96d84af8d866b9c645c1e98ea6945211b703
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-28 17:24:10 +0000

    change ' to "

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,7 +3,7 @@
 import sys
 import urllib.request
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
         print(response.info().get('X-Request-Id'))
