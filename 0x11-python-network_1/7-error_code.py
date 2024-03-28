a87587030d6dc1df1cf654ad35143ef168e61712
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-28 18:17:45 +0000

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" get the code of a request """
+import requests
+import sys
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    if (response.code >= 400):
+        print('Error code:', response.code)
+    else:
+        print(response.text)
