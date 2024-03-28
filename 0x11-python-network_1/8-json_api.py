3034296418285dbd236c3593001ce07f6da37685
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-28 23:51:36 +0000

    fix task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,7 +5,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    q = sys.argv[2]
+    q = ""
+    if len(sys.argv) > 2:
+        q = sys.argv[2]
     response = requests.post(url, data={'q': q})
     try:
         response = response.json()
