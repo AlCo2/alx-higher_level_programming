0eb4a55e4229c7ae838ffd8c65dea8d265fad296
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-28 16:55:18 +0000

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-
-print("Hello World")
+""" fetch data from a website using urllib"""
+import urllib.request
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    data = response.read()
+    print("Body response:")
+    print('- type: {}'.format(type(data)))
+    print('- content: {}'.format(data))
+    print('- utf8 content: {}'.format(data.decode('utf-8')))
