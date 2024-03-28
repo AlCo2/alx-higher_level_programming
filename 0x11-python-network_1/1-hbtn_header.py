33313dacca064997fa83728f12f819c9ace4cf4f
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-28 17:12:08 +0000

    add dict()

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,4 +4,5 @@ import sys
 import urllib.request
 
 with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.info()['X-Request-Id'])
+    print(dict(response.info()).get('X-Request-Id'))
+
